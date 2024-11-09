70610e5afd1cd76cddab73f6a34e1732b397ced9
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2024-11-09 08:32:12 -0600

    [Add]: manejo de imágenes

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,19 +1,37 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+
 import { FilesService } from './files.service';
+import { fileFilter, fileNamer } from './helpers';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/uploads',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    if (!file) {
+      throw new BadRequestException('Make sure that file is an image');
+    }
+
+    return {
+      fileName: file.originalname,
+    };
   }
 }
