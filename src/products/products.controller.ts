7215e17440bb93e24e994489d0b0eb9d005e18f2
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2024-12-06 19:10:17 -0600

    [Add]: Relacionar usuario con producto

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -12,15 +12,20 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDTO } from 'src/common/dtos/pagination.dto';
+import { PaginationDTO } from '../common/dtos/pagination.dto';
+import { Auth, GetUser } from '../auth/decorators';
+import { ValidRoles } from '../auth/interfaces';
+import { User } from '../auth/entities/user.entity';
 
 @Controller('products')
+// @Auth()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  @Auth()
+  create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
+    return this.productsService.create(createProductDto, user);
   }
 
   @Get()
@@ -34,14 +39,17 @@ export class ProductsController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin, ValidRoles.superUser)
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateProductDto: UpdateProductDto,
+    @GetUser() user: User,
   ) {
-    return this.productsService.update(id, updateProductDto);
+    return this.productsService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin, ValidRoles.superUser)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.productsService.remove(id);
   }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -13,6 +13,7 @@ import { DataSource, Repository } from 'typeorm';
 import { PaginationDTO } from '../common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
 import { Product, ProductImage } from './entities';
+import { User } from '../auth/entities/user.entity';
 
 @Injectable()
 export class ProductsService {
@@ -28,7 +29,7 @@ export class ProductsService {
     private readonly dataSource: DataSource,
   ) {}
 
-  async create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto, user: User) {
     try {
       const { images = [], ...productDetails } = createProductDto;
 
@@ -37,6 +38,7 @@ export class ProductsService {
         images: images.map((image) =>
           this.productImageRepository.create({ url: image }),
         ),
+        user,
       });
 
       await this.productRepository.save(product);
@@ -94,7 +96,7 @@ export class ProductsService {
     };
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto, user: User) {
     const { images, ...toUpdate } = updateProductDto;
 
     const product = await this.productRepository.preload({ id, ...toUpdate });
@@ -121,7 +123,9 @@ export class ProductsService {
       //   });
       // }
 
+      product.user = user;
       await queryRunner.manager.save(product);
+
       await queryRunner.commitTransaction();
       await queryRunner.release();
 
